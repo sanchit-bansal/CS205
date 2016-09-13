ef8112d859c0d33ea994519b98b04adbe199612d
voidestructor <sanchit42bansal@gmail.com>
2016-09-13 23:53:57 +0530

    updating Q5 and Q6

---
+++ Homeworks/HW2/Q5.scala
@@ -0,0 +1,20 @@
+object Q5 {
+  
+  def pivotSort(num1:List[Int], x: Int): List[Int]={
+     
+    if(num1 == Nil)
+      num1:+x
+    else if(num1.head<=x)
+      num1.head::pivotSort(num1.tail, x)
+    else {
+       
+      pivotSort(num1.tail, x):+num1.head
+      
+    }
+  
+}
+  def main(args: Array[String]): Unit = {
+    var list1 = List(1,2,4,3,9,16)
+    println(pivotSort(list1,10))
+  }
+}
